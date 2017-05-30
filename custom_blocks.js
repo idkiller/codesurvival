325eb2faa7601bad6c3b4cd2815ed33a3fa367e0
pius lee <idkiller@gmail.com>
2017-05-31 03:55:41 +0900

    define movements and add js code gen

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,1 +1,33 @@
 
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "move_forward",
+        "message0": "Forward",
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 225,
+        "tooltip": "move hero to forward"
+    },
+    {
+        "type": "turn_right",
+        "message0": "Turn Right",
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 225,
+        "tooltip": "turn right to hero"
+    },
+
+    {
+        "type": "turn_left",
+        "message0": "Turn Left",
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 225,
+        "tooltip": "turn left to hero"
+    },
+]);
+
+
+Blockly.JavaScript['move_forward'] = function(block) {
+  return 'game_hero_forward();\n';
+};
--- game.js
+++ game.js
@@ -13,77 +13,125 @@ Ctx = null;
 
 _unit = {};
 
-function create_grid()
+_survival_log_element = document.getElementById("survival_log");
+
+function log(str)
 {
+    _survival_log_element.value += str + "\n";
+}
+
+function create_grid() {
     var w, h;
     Ctx.setStart();
-    for (i=0; i<11; i++)
-    {
+    for (i = 0; i < 11; i++) {
         w = i * CellWidth;
         h = i * CellHeight;
-        Ctx.path("M0,"+w+"L"+CanvasWidth+","+w+"Z");
-        Ctx.path("M"+h+",0L"+h+","+CanvasHeight+"Z");
+        Ctx.path("M0," + w + "L" + CanvasWidth + "," + w + "Z");
+        Ctx.path("M" + h + ",0L" + h + "," + CanvasHeight + "Z");
     }
     var grid = Ctx.setFinish();
-    grid.attr({stroke:"gray"});
+    grid.attr({ stroke: "gray" });
     return grid;
 }
 
-function create_unit(src)
-{
-    if (!_unit[src])
-    {
+function create_unit(src) {
+    if (!_unit[src]) {
         _unit[src] = Ctx.image(src, -UnitWidth, -UnitHeight, UnitWidth, UnitHeight);
         _unit[src].hide();
     }
     return _unit[src].clone();
 }
 
-function create_monster()
-{
+function create_monster() {
     return create_unit("img/monster.png");
 }
 
-function create_traps()
-{
+function create_traps() {
     return create_unit("img/trap.png");
 }
 
-function create_treasure()
-{
+function create_treasure() {
     return create_unit("img/treasure.png");
 }
 
-function create_hero()
-{
+function create_hero() {
     return create_unit("img/hero.png");
 }
 
 function _X(x) { return x * CellWidth; }
 function _Y(y) { return y * CellHeight; }
 
-function Unit(element, px, py)
+function unit_move(unit, rx, ry)
 {
-    return {unit: element, x: px, y: py,
-        move: function(x, y){
-            this.unit.translate(_X(x), _(y));
+    var bb = unit.u.getBBox();
+    var cx = unit.x + rx,
+        cy = unit.y + ry;
+    if (cx > 10 || cx < 1 || cy < 1 || cy > 10) return;
+    var elem = unit.u;
+    var tx = _X(cx-2);
+    var ty = _Y(cy-2);
+    console.log(tx + ", " + ty);
+    elem.animate({
+        x: tx, y: ty
+    }, 200, "linear", null);
+    unit.x = cx;
+    unit.y = cy;
+}
+
+function Unit(element, px, py) {
+    return {
+        u: element, x: px, y: py, dx: 1, dy: 0, dir: 1, trans:element.attr('transform'), rotation:0,
+        move: function () {
+
+            if (this.x + this.dx > 10) {
+                this.dx = -1;
+            }
+            if (this.x < 2) {
+                this.dx = 1;
+            }
+
+            this.u.translate(_X(this.dx), 0);
+            this.x += this.dx;
+        },
+        forward: function() {
+            switch(this.dir)
+            {
+                // 1: downward, 2: left, 3: upward, 4: right
+                case 1: unit_move(this, 0, 1); break;
+                case 2: unit_move(this, -1, 0); break;
+                case 3: unit_move(this, 0, -1); break;
+                case 4: unit_move(this, 1, 0); break;
+            }
+        },
+        turn_right: function() {
+            var bb = this.u.getBBox();
+            var cx = bb.x + bb.width / 2, cy = bb.y + bb.height / 2;
+            this.dir = this.dir == 4 ? 1 : this.dir+1;
+            this.rotation += 90;
+            this.u.animate({transform:this.trans+"R"+(this.rotation)+","+cx+","+cy}, 200, "linear", null);
+        },
+        turn_left: function() {
+            var bb = this.u.getBBox();
+            var cx = bb.x + bb.width / 2, cy = bb.y + bb.height / 2;
+            this.dir = this.dir == 1 ? 4 : this.dir-1;
+            this.rotation -= 90;
+            this.u.animate({transform:this.trans+"R"+(this.rotation)+","+cx+","+cy}, 200, "linear", null);
         }
     };
 }
 
-function game_run()
-{
-    Monsters.forEach(function(unit, index, arr){
+function game_run() {
+    Monsters.forEach(function (unit, index, arr) {
+        unit.move();
     });
 }
 
-function game_init(monsters, traps, treasures)
-{
-    var i=0, p=0;
+function game_init(monsters, traps, treasures) {
+    var i = 0, p = 0;
 
     create_grid();
 
-    traps.forEach(function(xy, index, arr){
+    traps.forEach(function (xy, index, arr) {
         var x = xy[0], y = xy[1];
         var t = create_traps();
         t.translate(_X(x), _Y(y));
@@ -91,7 +139,7 @@ function game_init(monsters, traps, treasures)
         Traps.push(Unit(t));
     });
 
-    monsters.forEach(function(xy, index, arr){
+    monsters.forEach(function (xy, index, arr) {
         var x = xy[0], y = xy[1];
         var m = create_monster();
         m.translate(_X(x), _Y(y));
@@ -99,7 +147,7 @@ function game_init(monsters, traps, treasures)
         Monsters.push(Unit(m, x, y));
     });
 
-    treasures.forEach(function(xy, index, arr){
+    treasures.forEach(function (xy, index, arr) {
         var x = xy[0], y = xy[1];
         var u = create_treasure();
         u.translate(_X(x), _Y(y));
@@ -107,14 +155,15 @@ function game_init(monsters, traps, treasures)
         Treasures.push(Unit(u, x, y));
     });
 
-    Hero = create_hero();
-    Hero.translate(_X(1), _Y(1));
-    Hero.show();
+    var hero = create_hero();
+    hero.translate(_X(1), _Y(1));
+    hero.show();
+    Hero = Unit(hero, 1, 1);
 }
 
 function game_main(canvas, monsters, traps, treasures) {
     Raphael(
-        function() {
+        function () {
             var canvasElement = document.getElementById(canvas);
             Ctx = Raphael(canvas);
             CanvasWidth = Ctx.width;
@@ -125,4 +174,17 @@ function game_main(canvas, monsters, traps, treasures) {
             UnitHeight = CellHeight - 4;
             game_init(monsters, traps, treasures);
         });
+}
+
+function game_start(code)
+{
+    console.log(code);
+    var runner = function () {
+        game_run();
+        setTimeout(runner, 300);
+    };
+}
+
+function game_hero_forward()
+{
 }
