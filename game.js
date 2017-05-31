7782b786d52b5920eeed72362ab3728695f9c9b7
pius.lee <pius.lee@samsung.com>
2017-05-31 16:11:36 +0900

    fix monster to move and add stop game button

--- game.js
+++ game.js
@@ -15,9 +15,29 @@ _unit = {};
 
 _survival_log_element = document.getElementById("survival_log");
 
-function log(str)
-{
+_seed = 1;
+
+_game_running = false;
+
+function log(str) {
     _survival_log_element.value += str + "\n";
+}
+
+function random_seed(seed) {
+    _seed = seed;
+}
+
+function random() {
+    var x = Math.sin(_seed++) * 10000;
+    return x - Math.floor(x);
+}
+
+function random_between(a, b) {
+    return ~~((random() / 1) * (b - a) + a);
+}
+
+function random_coin() {
+    return ~~(random() * 10) % 2 == 0;
 }
 
 function create_grid() {
@@ -61,26 +81,49 @@ function create_hero() {
 function _X(x) { return x * CellWidth; }
 function _Y(y) { return y * CellHeight; }
 
-function unit_move(unit, rx, ry)
-{
+function unit_transform(unit, dx, dy) {
+    var cx = -CellWidth / 2, cy = -CellHeight / 2;
+    var tstr = unit.trans + ["t", dx, dy] + ["r" + unit.rotation, cx, cy];
+    //console.log(tstr);
+    //console.log(unit.dir);
+    return tstr;
+}
+
+function unit_move(unit, rx, ry) {
     var bb = unit.u.getBBox();
     var cx = unit.x + rx,
         cy = unit.y + ry;
     if (cx > 10 || cx < 1 || cy < 1 || cy > 10) return;
     var elem = unit.u;
-    var tx = _X(cx-2);
-    var ty = _Y(cy-2);
-    console.log(tx + ", " + ty);
+    var tx = _X(cx - 1);
+    var ty = _Y(cy - 1);
     elem.animate({
-        x: tx, y: ty
+        transform: unit_transform(unit, tx, ty)
     }, 200, "linear", null);
     unit.x = cx;
     unit.y = cy;
 }
 
+function unit_dir(unit) {
+    return (Math.abs((unit.rotation + 360) / 90) % 4) + 1;
+}
+
+function nextpos(dir) {
+    // 1: downward, 2: left, 3: upward, 4: right
+    switch (dir) {
+        case 1: return [0, 1];
+        case 2: return [-1, 0];
+        case 3: return [0, -1];
+        case 4: return [1, 0];
+    }
+    return [0, 0];
+}
+
 function Unit(element, px, py) {
     return {
-        u: element, x: px, y: py, dx: 1, dy: 0, dir: 1, trans:element.attr('transform'), rotation:0,
+        u: element, x: px, y: py, dx: 1, dy: 0,
+        trans: element.attr('transform'),
+        rotation: 0, tx: 0, ty: 0,
         move: function () {
 
             if (this.x + this.dx > 10) {
@@ -93,36 +136,84 @@ function Unit(element, px, py) {
             this.u.translate(_X(this.dx), 0);
             this.x += this.dx;
         },
-        forward: function() {
-            switch(this.dir)
-            {
-                // 1: downward, 2: left, 3: upward, 4: right
-                case 1: unit_move(this, 0, 1); break;
-                case 2: unit_move(this, -1, 0); break;
-                case 3: unit_move(this, 0, -1); break;
-                case 4: unit_move(this, 1, 0); break;
-            }
+        forward: function () {
+            var dir = unit_dir(this);
+            var np = nextpos(dir)
+            unit_move(this, np[0], np[1]);
+            //console.log(this.rotation);
+            //console.log(dir);
         },
-        turn_right: function() {
-            var bb = this.u.getBBox();
-            var cx = bb.x + bb.width / 2, cy = bb.y + bb.height / 2;
-            this.dir = this.dir == 4 ? 1 : this.dir+1;
+        turn_right: function () {
             this.rotation += 90;
-            this.u.animate({transform:this.trans+"R"+(this.rotation)+","+cx+","+cy}, 200, "linear", null);
+            unit_move(this, 0, 0);
         },
-        turn_left: function() {
-            var bb = this.u.getBBox();
-            var cx = bb.x + bb.width / 2, cy = bb.y + bb.height / 2;
-            this.dir = this.dir == 1 ? 4 : this.dir-1;
+        turn_left: function () {
             this.rotation -= 90;
-            this.u.animate({transform:this.trans+"R"+(this.rotation)+","+cx+","+cy}, 200, "linear", null);
+            unit_move(this, 0, 0);
+        },
+        detect: function () {
+            var somthing = null;
+            var np = nextpos(unit_dir(this));
+            var tx = np[0] + this.x;
+            var ty = np[1] + this.y;
+
+            Monsters.every(function (unit) {
+                if (unit.x == tx && unit.y == ty) {
+                    somthing = "monster";
+                    return false;
+                }
+                return true;
+            });
+            if (somthing != null) return somthing;
+
+            Traps.every(function (unit) {
+                if (unit.x == tx && unit.y == ty) {
+                    somthing = "traps";
+                    return false;
+                }
+                return true;
+            });
+            if (somthing != null) return somthing;
+
+            if (tx > 10 || tx < 1 || ty < 1 || ty > 10)
+                somthing = "wall";
+            if (somthing != null) return somthing;
+
+            Treasures.every(function (unit) {
+                if (unit.x == tx && unit.y == ty) {
+                    somthing = "treasure";
+                    return false;
+                }
+                return true;
+            });
+
+            return somthing;
         }
     };
 }
 
 function game_run() {
     Monsters.forEach(function (unit, index, arr) {
-        unit.move();
+        var blocker = unit.detect();
+        if (blocker == null) {
+            unit.forward();
+        }
+        else if (blocker == "wall") {
+            unit.turn_right();
+            unit.turn_right();
+        }
+        else if (blocker == "traps") {
+            if (random_coin()) unit.turn_left();
+            else unit.turn_right();
+        }
+        else if (blocker == "monster") {
+            if (random_coin()) unit.turn_left();
+            else unit.turn_right();
+        }
+        else if (blocker == "treasure") {
+            if (random_coin()) unit.turn_left();
+            else unit.turn_right();
+        }
     });
 }
 
@@ -136,15 +227,24 @@ function game_init(monsters, traps, treasures) {
         var t = create_traps();
         t.translate(_X(x), _Y(y));
         t.show();
-        Traps.push(Unit(t));
+        Traps.push(Unit(t, x, y));
     });
 
     monsters.forEach(function (xy, index, arr) {
         var x = xy[0], y = xy[1];
         var m = create_monster();
-        m.translate(_X(x), _Y(y));
+        m.translate(_X(1), _Y(1));
         m.show();
-        Monsters.push(Unit(m, x, y));
+        var unit = Unit(m, 1, 1);
+
+        for (i = 0; i < y; i++) {
+            unit.forward();
+        }
+        unit.turn_left();
+        for (i = 0; i < x; i++) {
+            unit.forward();
+        }
+        Monsters.push(unit);
     });
 
     treasures.forEach(function (xy, index, arr) {
@@ -176,15 +276,20 @@ function game_main(canvas, monsters, traps, treasures) {
         });
 }
 
-function game_start(code)
-{
+function game_start(code) {
     console.log(code);
     var runner = function () {
         game_run();
-        setTimeout(runner, 300);
+        if (_game_running)
+            setTimeout(runner, 300);
     };
+    _game_running = true;
+    runner();
 }
 
-function game_hero_forward()
-{
+function game_stop() {
+    _game_running = false;
+}
+
+function game_hero_forward() {
 }
