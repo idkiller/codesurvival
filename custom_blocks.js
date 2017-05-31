d590afb3284371d2dab2ff7483825fba1e15d41d
pius.lee <pius.lee@samsung.com>
2017-05-31 22:27:38 +0900

    change many.... things..

--- custom_blocks.js
+++ custom_blocks.js
@@ -5,7 +5,7 @@ Blockly.defineBlocksWithJsonArray([
         "message0": "Forward",
         "previousStatement": null,
         "nextStatement": null,
-        "colour": 225,
+        "colour": 330,
         "tooltip": "move hero to forward"
     },
     {
@@ -13,7 +13,7 @@ Blockly.defineBlocksWithJsonArray([
         "message0": "Turn Right",
         "previousStatement": null,
         "nextStatement": null,
-        "colour": 225,
+        "colour": 330,
         "tooltip": "turn right to hero"
     },
 
@@ -22,12 +22,76 @@ Blockly.defineBlocksWithJsonArray([
         "message0": "Turn Left",
         "previousStatement": null,
         "nextStatement": null,
-        "colour": 225,
+        "colour": 330,
         "tooltip": "turn left to hero"
+    },
+
+    {
+        "type": "detect",
+        "message0": "Detect",
+        "colour": 120,
+        "output": "String",
+        "tooltip": "detect what is front of hero"
+    },
+    {
+        "type": "blocker_wall",
+        "message0": "Wall",
+        "colour": 120,
+        "output": "String",
+        "tooltip": "Wall"
+    },
+    {
+        "type": "blocker_monster",
+        "message0": "Monster",
+        "colour": 120,
+        "output": "String",
+        "tooltip": "Monster"
+    },
+    {
+        "type": "blocker_trap",
+        "message0": "Trap",
+        "colour": 120,
+        "output": "String",
+        "tooltip": "Trap"
+    },
+    {
+        "type": "blocker_treasure",
+        "message0": "Treasure",
+        "colour": 120,
+        "output": "String",
+        "tooltip": "Treasure"
     },
 ]);
 
 
-Blockly.JavaScript['move_forward'] = function(block) {
-  return 'game_hero_forward();\n';
+Blockly.JavaScript['move_forward'] = function (block) {
+    return 'Hero.forward();\n';
 };
+
+Blockly.JavaScript['turn_right'] = function (block) {
+    return 'Hero.turn_right();\n';
+};
+
+Blockly.JavaScript['turn_left'] = function (block) {
+    return 'Hero.turn_left();\n';
+}
+
+Blockly.JavaScript['detect'] = function (block) {
+    return ['Hero.detect()', Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['blocker_wall'] = function (block) {
+    return ["\'wall\'", Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['blocker_monster'] = function (block) {
+    return ["\'monster\'", Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['blocker_trap'] = function (block) {
+    return ["\'trap\'", Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['blocker_treasure'] = function (block) {
+    return ["\'treasure\'", Blockly.JavaScript.ORDER_NONE];
+}
--- game.js
+++ game.js
@@ -13,18 +13,34 @@ Ctx = null;
 
 _unit = {};
 
-_survival_log_element = document.getElementById("survival_log");
+_survival_log_element = null;
 
 _seed = 1;
+_init_seed = 1;
 
 _game_running = false;
 
+Score = 0;
+
+_grid = null;
+
+_loop = null;
+
+_game_grid = [];
+
 function log(str) {
-    _survival_log_element.value += str + "\n";
+    if (!_survival_log_element)
+        _survival_log_element = document.getElementById("survival_log");
+    _survival_log_element.value = str + "\n";
 }
 
-function random_seed(seed) {
-    _seed = seed;
+function set_random_seed(seed) {
+    _init_seed = seed;
+    reset_random();
+}
+
+function reset_random() {
+    _seed = _init_seed;
 }
 
 function random() {
@@ -83,7 +99,10 @@ function _Y(y) { return y * CellHeight; }
 
 function unit_transform(unit, dx, dy) {
     var cx = -CellWidth / 2, cy = -CellHeight / 2;
-    var tstr = unit.trans + ["t", dx, dy] + ["r" + unit.rotation, cx, cy];
+    var tstr = unit.trans + ["t" + dx, dy] + ["r" + unit.rotation, cx, cy];
+    if (unit.scale != 1) {
+        tstr += ['s' + unit.scale, unit.scale];
+    }
     //console.log(tstr);
     //console.log(unit.dir);
     return tstr;
@@ -119,40 +138,65 @@ function nextpos(dir) {
     return [0, 0];
 }
 
-function Unit(element, px, py) {
-    return {
-        u: element, x: px, y: py, dx: 1, dy: 0,
+function unit_collision(a, x, y) {
+    if (!a.on_collision) return true;
+    var tx = a.x + x, ty = a.y + y;
+    if (tx < 1 || tx > 10 || ty < 1 || ty > 10) {
+        return a.on_collision("wall", null);
+    }
+    var b = _game_grid[ty][tx];
+    if (b) {
+        console.log(b);
+        return a.on_collision(b.type, b);
+    }
+    return true;
+}
+
+function Unit(t, element, px, py, collision_cb) {
+    var __unit = {
+        type: t,
+        u: element, x: px, y: py,
         trans: element.attr('transform'),
-        rotation: 0, tx: 0, ty: 0,
-        move: function () {
-
-            if (this.x + this.dx > 10) {
-                this.dx = -1;
-            }
-            if (this.x < 2) {
-                this.dx = 1;
-            }
-
-            this.u.translate(_X(this.dx), 0);
-            this.x += this.dx;
+        rotation: 0, scale: 1,
+        blocker: null,
+        life: 3,
+        initialized: false,
+        on_collision: collision_cb,
+        is_dead: false,
+        init: function () {
+            _game_grid[this.y][this.x] = this;
+            this.initialized = true;
         },
         forward: function () {
             var dir = unit_dir(this);
-            var np = nextpos(dir)
-            unit_move(this, np[0], np[1]);
-            //console.log(this.rotation);
-            //console.log(dir);
+            var np = nextpos(dir);
+            var c = true;
+            if (this.initialized) {
+                c = unit_collision(this, np[0], np[1]);
+            }
+
+            if (c) {
+                _game_grid[this.y][this.x] = null;
+                unit_move(this, np[0], np[1]);
+                _game_grid[this.y][this.x] = this;
+            }
+
+            this.blocker = null;
         },
         turn_right: function () {
             this.rotation += 90;
             unit_move(this, 0, 0);
+            this.blocker = null;
         },
         turn_left: function () {
             this.rotation -= 90;
             unit_move(this, 0, 0);
+            this.blocker = null;
         },
         detect: function () {
-            var somthing = null;
+            var somthing = this.blocker;
+            if (somthing != null) return somthing;
+
             var np = nextpos(unit_dir(this));
             var tx = np[0] + this.x;
             var ty = np[1] + this.y;
@@ -168,7 +212,7 @@ function Unit(element, px, py) {
 
             Traps.every(function (unit) {
                 if (unit.x == tx && unit.y == ty) {
-                    somthing = "traps";
+                    somthing = "trap";
                     return false;
                 }
                 return true;
@@ -187,47 +231,83 @@ function Unit(element, px, py) {
                 return true;
             });
 
+            this.blocker = somthing;
+
             return somthing;
+        },
+        dead: function () {
+            this.scale = 1.2;
+            unit_move(this, 0, 0);
+            this.scale = 0.1;
+            unit_move(this, 0, 0);
+
+            this.u.remove();
+            _game_grid[this.y][this.x] = null;
+            this.is_dead = true;
         }
     };
+
+    __unit.init();
+
+    return __unit;
 }
 
 function game_run() {
     Monsters.forEach(function (unit, index, arr) {
-        var blocker = unit.detect();
-        if (blocker == null) {
-            unit.forward();
-        }
-        else if (blocker == "wall") {
-            unit.turn_right();
-            unit.turn_right();
-        }
-        else if (blocker == "traps") {
-            if (random_coin()) unit.turn_left();
-            else unit.turn_right();
-        }
-        else if (blocker == "monster") {
-            if (random_coin()) unit.turn_left();
-            else unit.turn_right();
-        }
-        else if (blocker == "treasure") {
-            if (random_coin()) unit.turn_left();
-            else unit.turn_right();
-        }
+        unit.forward();
     });
 }
 
-function game_init(monsters, traps, treasures) {
+function game_init() {
     var i = 0, p = 0;
 
-    create_grid();
+    if (!_grid) _grid = create_grid();
+
+    _game_grid = [];
+    for (i = 0; i < 11; i++) _game_grid.push(new Array(11));
+
+    var units = [];
+    var monster_count = random_between(3, 6);
+    var monsters = [], monster = null;
+    for (i = 0; i < monster_count; i++) {
+        monster = null;
+        do {
+            monster = [random_between(1, 10), random_between(1, 10)];
+        }
+        while (units.some(function (x) { return x[0] == monster[0] && x[1] == monster[1]; }))
+        monsters.push(monster);
+        units.push(monster);
+    }
+    var trap_count = random_between(3, 6);
+    var traps = [], trap = null;
+    for (i = 0; i < trap_count; i++) {
+        trap = null;
+        do {
+            trap = [random_between(1, 10), random_between(1, 10)];
+        }
+        while (units.some(function (x) { return x[0] == trap[0] && x[1] == trap[1]; }))
+        traps.push(trap);
+        units.push(trap);
+    }
+    var treasure_count = random_between(3, 6);
+    var treasures = [], treasure = null;
+    for (i = 0; i < treasure_count; i++) {
+        treasure = null;
+        do {
+            treasure = [random_between(1, 10), random_between(1, 10)];
+        }
+        while (units.some(function (x) { return x[0] == treasure[0] && x[1] == treasure[1]; }))
+        treasures.push(treasure);
+        units.push(treasure);
+    }
 
     traps.forEach(function (xy, index, arr) {
         var x = xy[0], y = xy[1];
         var t = create_traps();
         t.translate(_X(x), _Y(y));
         t.show();
-        Traps.push(Unit(t, x, y));
+        var trap = Unit('trap', t, x, y);
+        Traps.push(trap);
     });
 
     monsters.forEach(function (xy, index, arr) {
@@ -235,7 +315,8 @@ function game_init(monsters, traps, treasures) {
         var m = create_monster();
         m.translate(_X(1), _Y(1));
         m.show();
-        var unit = Unit(m, 1, 1);
+        var unit = Unit('monster', m, 1, 1);
+        unit.initialized = false;
 
         for (i = 0; i < y; i++) {
             unit.forward();
@@ -244,6 +325,28 @@ function game_init(monsters, traps, treasures) {
         for (i = 0; i < x; i++) {
             unit.forward();
         }
+        unit.initialized = true;
+
+        unit.on_collision = function (type, b) {
+            if (type == "wall") {
+                this.turn_right();
+                this.turn_right();
+                return false;
+            }
+            else if (type == "trap" || type == "monster" || type == "treasure") {
+                if (random_coin()) unit.turn_left();
+                else unit.turn_right();
+                return false;
+            }
+            else if (type == "hero") {
+                if (b.life-- < 1) {
+                    b.dead();
+                }
+                return false;
+            }
+            return true;
+        }
+
         Monsters.push(unit);
     });
 
@@ -252,16 +355,17 @@ function game_init(monsters, traps, treasures) {
         var u = create_treasure();
         u.translate(_X(x), _Y(y));
         u.show();
-        Treasures.push(Unit(u, x, y));
+        var unit = Unit('treasure', u, x, y);
+        Treasures.push(unit);
     });
 
     var hero = create_hero();
     hero.translate(_X(1), _Y(1));
     hero.show();
-    Hero = Unit(hero, 1, 1);
+    Hero = Unit('hero', hero, 1, 1);
 }
 
-function game_main(canvas, monsters, traps, treasures) {
+function game_main(canvas) {
     Raphael(
         function () {
             var canvasElement = document.getElementById(canvas);
@@ -272,16 +376,42 @@ function game_main(canvas, monsters, traps, treasures) {
             CellHeight = CanvasHeight / 10;
             UnitWidth = CellWidth - 4;
             UnitHeight = CellHeight - 4;
-            game_init(monsters, traps, treasures);
+            game_init();
         });
 }
 
 function game_start(code) {
     console.log(code);
+
+    reset_random();
+
+    if (_loop) {
+        clearTimeout(_loop);
+        _loop = null;
+    }
+
+    var remover = function (m) { m.u.remove(); };
+    Monsters.forEach(remover);
+    Traps.forEach(remover);
+    Treasures.forEach(remover);
+    Monsters = [];
+    Traps = [];
+    Treasures = [];
+    if (Hero != null) Hero.u.remove();
+    Hero = null;
+
+    game_init();
+
     var runner = function () {
         game_run();
+        eval(code);
+        if (Hero.is_dead) {
+            _game_running = false;
+        }
         if (_game_running)
-            setTimeout(runner, 300);
+            _loop = setTimeout(runner, 300);
+
+        log(__debug());
     };
     _game_running = true;
     runner();
@@ -291,5 +421,21 @@ function game_stop() {
     _game_running = false;
 }
 
-function game_hero_forward() {
+function __debug() {
+    var i = 0, j = 0, str = "";
+    for (i = 1; i < 11; i++) {
+        for (j = 1; j < 11; j++) {
+            if (_game_grid[i][j]) {
+                var t = _game_grid[i][j].type;
+                if (t == "monster") str += '1 ';
+                else if (t == "trap") str += 't ';
+                else if (t == "treasure") str += 'w ';
+                else if (t == "hero") str += '* ';
+                else str += 'x ';
+            }
+            else str += '0 ';
+        }
+        str += "\n";
+    }
+    return str;
 }
